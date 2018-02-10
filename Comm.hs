c0a75f5f946c8d5f6b2adfec502f22273ab5d3ec
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2018-02-10 22:43:26 +0600

    Prints the common lines beween two files

--- Comm.hs
+++ Comm.hs
@@ -3,7 +3,22 @@ import System.IO
 
 main :: IO ()
 main = do
-        fileName <- getLine
-        handle <- openFile fileName ReadMode
-        contents <- hGetContents handle
-        putStr contents
+        args <- getArgs
+        firstFileHandle <- openFile (args !! 0) ReadMode
+        secondFileHandle <- openFile (args !! 1) ReadMode
+        firstFileContents <- hGetContents firstFileHandle
+        secondFileContents <- hGetContents secondFileHandle
+        let (firstUnique, secondUnique, common) = extractCommonAndUnique (lines firstFileContents) (lines secondFileContents)
+--        rs <- sequence $ map putStrLn $ reverse $ common
+--        print rs
+        mapM_ putStrLn $ reverse common
+
+extractCommonAndUnique list1 list2 = extractCommonAndUniqueHelper list1 list2 ([], [], [])
+
+extractCommonAndUniqueHelper [] [] (aUnique, bUnique, common) = (aUnique, bUnique, common)
+extractCommonAndUniqueHelper [] (b:bs) (aUnique, bUnique, common) = extractCommonAndUniqueHelper [] bs (aUnique, (b:bUnique), common)
+extractCommonAndUniqueHelper (a:as) [] (aUnique, bUnique, common) = extractCommonAndUniqueHelper as [] ((a:aUnique), bUnique, common)
+extractCommonAndUniqueHelper (a:as) (b:bs) (aUnique, bUnique, common)
+    | (a < b) = extractCommonAndUniqueHelper as (b:bs) ((a:aUnique), bUnique, common)
+    | (a > b) = extractCommonAndUniqueHelper (a:as) bs (aUnique, (b:bUnique), common)
+    | otherwise = extractCommonAndUniqueHelper as bs (aUnique, bUnique, (a:common))
