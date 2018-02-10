eba12ec5dc9f50f8704c20c8504d083fa98a50c8
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2018-02-10 17:38:20 +0600

    Initial commit

---
+++ Comm.hs
@@ -0,0 +1,12 @@
+--
+-- Copyright (c) 2018 Ibraheem Moosa
+-- GPL version 2 or later (see http://www.gnu.org/copyleft/gpl.html)
+--
+import System.Environment
+
+main :: IO ()
+main = do
+        input <- getLine
+        putStrLn input
+
+
