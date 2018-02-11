8f02ddc60f048f423527b954d256566e3c8a7a1c
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2018-02-11 21:49:54 +0600

    implemented wthout reverse and passing result as parameter

--- Comm.hs
+++ Comm.hs
@@ -11,14 +11,26 @@ main = do
         let (firstUnique, secondUnique, common) = extractCommonAndUnique (lines firstFileContents) (lines secondFileContents)
 --        rs <- sequence $ map putStrLn $ reverse $ common
 --        print rs
-        mapM_ putStrLn $ reverse common
+        mapM_ putStrLn common
 
+extractCommonAndUnique a [] = (a, [], [])
+extractCommonAndUnique [] b = ([], b, [])
+extractCommonAndUnique (a:as) (b:bs)
+    | a < b = ((a:aRest1), bRest1, cRest1)
+    | a > b = (aRest2, (b:bRest2), cRest2)
+    | otherwise = (aRest3, bRest3, (a:cRest3))
+    where 
+        (aRest1, bRest1, cRest1) = extractCommonAndUnique as (b:bs)
+        (aRest2, bRest2, cRest2) = extractCommonAndUnique (a:as) bs
+        (aRest3, bRest3, cRest3) = extractCommonAndUnique as bs
+{-
 extractCommonAndUnique list1 list2 = extractCommonAndUniqueHelper list1 list2 ([], [], [])
 
-extractCommonAndUniqueHelper [] [] (aUnique, bUnique, common) = (aUnique, bUnique, common)
+extractCommonAndUniqueHelper [] [] result = result
 extractCommonAndUniqueHelper [] (b:bs) (aUnique, bUnique, common) = extractCommonAndUniqueHelper [] bs (aUnique, (b:bUnique), common)
 extractCommonAndUniqueHelper (a:as) [] (aUnique, bUnique, common) = extractCommonAndUniqueHelper as [] ((a:aUnique), bUnique, common)
 extractCommonAndUniqueHelper (a:as) (b:bs) (aUnique, bUnique, common)
     | (a < b) = extractCommonAndUniqueHelper as (b:bs) ((a:aUnique), bUnique, common)
     | (a > b) = extractCommonAndUniqueHelper (a:as) bs (aUnique, (b:bUnique), common)
     | otherwise = extractCommonAndUniqueHelper as bs (aUnique, bUnique, (a:common))
+-}
