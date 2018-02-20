8f0fd52851db40a41b49832ff86c51b6889c8c2f
Matt Parsons <parsonsmatt@gmail.com>
2018-02-20 14:58:07 -0700

    Whitespace and indentation changes
    It's somewhat unidiomatic to use 8 space indent in Haskell code.
    Typically, 4 or 2 space are common. I like four space, but two space
    works great too.
    
    When doing a hanging `do`, it's best to indent it only four spaces. That
    makes the code a bit clearer to read, and less indented off the side.

--- Comm.hs
+++ Comm.hs
@@ -2,49 +2,41 @@ import System.Environment
 import System.IO
 import Data.Maybe
 
-
 main :: IO ()
 main = do
-        args <- getArgs
-        let argLength = length args
-        firstFileHandle <- openFile (last $ init args) ReadMode
-        secondFileHandle <- openFile (last args) ReadMode
-        firstFileContents <- hGetContents firstFileHandle
-        secondFileContents <- hGetContents secondFileHandle
-        let configuration = extractConfigurationFromArgs $ take (argLength - 2) args
-        let a = elem '1' configuration 
-        let b = elem '2' configuration
-        let c = elem '3' configuration
-        printComm '\t' (a, b, c) $ extractCommonAndUnique (lines firstFileContents) (lines secondFileContents)
-
+    args <- getArgs
+    let argLength = length args
+    firstFileHandle <- openFile (last $ init args) ReadMode
+    secondFileHandle <- openFile (last args) ReadMode
+    firstFileContents <- hGetContents firstFileHandle
+    secondFileContents <- hGetContents secondFileHandle
+    let configuration = extractConfigurationFromArgs $ take (argLength - 2) args
+    let a = elem '1' configuration
+    let b = elem '2' configuration
+    let c = elem '3' configuration
+    printComm '\t' (a, b, c) $ extractCommonAndUnique (lines firstFileContents) (lines secondFileContents)
 
 extractConfigurationFromArgs :: [String] -> String
 extractConfigurationFromArgs args = foldl (++) "" $ map tail $ filter (\x -> head x == '-') args
 
-
 data CommonOrUnique = FirstUnique | SecondUnique | Common
 
-
 printComm :: Char -> (Bool, Bool, Bool) -> [(String, CommonOrUnique)] -> IO ()
-
 printComm _ _ [] = return ()
 printComm delimiter omit@(omitFirstUnique, _, _) ((a, FirstUnique):as) = do
-                                                                    if omitFirstUnique then return () else putStrLn $ (replicate 0 delimiter) ++ a
-                                                                    printComm delimiter omit as
+    if omitFirstUnique then return () else putStrLn $ (replicate 0 delimiter) ++ a
+    printComm delimiter omit as
 printComm delimiter omit@(_, omitSecondUnique, _) ((a, SecondUnique):as) = do
-                                                                      if omitSecondUnique then return () else putStrLn $ (replicate 1 delimiter) ++ a
-                                                                      printComm delimiter omit as
+    if omitSecondUnique then return () else putStrLn $ (replicate 1 delimiter) ++ a
+    printComm delimiter omit as
 printComm delimiter omit@(_, _, omitCommon) ((a, Common):as) = do
-                                                           if omitCommon then return () else putStrLn $ (replicate 2 delimiter) ++ a
-                                                           printComm delimiter omit as
-
+    if omitCommon then return () else putStrLn $ (replicate 2 delimiter) ++ a
+    printComm delimiter omit as
 
 extractCommonAndUnique :: (Ord a) => [a] -> [a] -> [(a, CommonOrUnique)]
-
 extractCommonAndUnique a [] = map (\x -> (x, FirstUnique)) a
 extractCommonAndUnique [] b = map (\x -> (x, SecondUnique)) b
 extractCommonAndUnique (a:as) (b:bs)
     | a < b = (a, FirstUnique):extractCommonAndUnique as (b:bs)
     | a > b = (b, SecondUnique):extractCommonAndUnique (a:as) bs
     | otherwise = (a, Common):extractCommonAndUnique as bs
-
