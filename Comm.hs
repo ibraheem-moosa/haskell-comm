56d61fa80066d726da8ec6dd1d4e6c90b172fc1a
Matt Parsons <parsonsmatt@gmail.com>
2018-02-20 15:03:28 -0700

    Formatting changes, readability
    I've changed the formatting so that no line is over 80 columns. I've
    used vertical alignment of operators and `=` to help break apart the
    code into more lines.

--- Comm.hs
+++ Comm.hs
@@ -10,33 +10,47 @@ main = do
     secondFileHandle <- openFile (last args) ReadMode
     firstFileContents <- hGetContents firstFileHandle
     secondFileContents <- hGetContents secondFileHandle
-    let configuration = extractConfigurationFromArgs $ take (argLength - 2) args
+    let configuration = extractConfigurationFromArgs
+                      $ take (argLength - 2) args
     let a = elem '1' configuration
     let b = elem '2' configuration
     let c = elem '3' configuration
-    printComm '\t' (a, b, c) $ extractCommonAndUnique (lines firstFileContents) (lines secondFileContents)
+    printComm '\t' (a, b, c)
+        $ extractCommonAndUnique
+            (lines firstFileContents)
+            (lines secondFileContents)
 
 extractConfigurationFromArgs :: [String] -> String
-extractConfigurationFromArgs args = foldl (++) "" $ map tail $ filter (\x -> head x == '-') args
+extractConfigurationFromArgs args =
+    foldl (++) ""
+        $ map tail
+        $ filter (\x -> head x == '-')
+        $ args
 
 data CommonOrUnique = FirstUnique | SecondUnique | Common
 
 printComm :: Char -> (Bool, Bool, Bool) -> [(String, CommonOrUnique)] -> IO ()
 printComm _ _ [] = return ()
 printComm delimiter omit@(omitFirstUnique, _, _) ((a, FirstUnique):as) = do
-    if omitFirstUnique then return () else putStrLn $ (replicate 0 delimiter) ++ a
+    if omitFirstUnique
+        then return ()
+        else putStrLn $ (replicate 0 delimiter) ++ a
     printComm delimiter omit as
 printComm delimiter omit@(_, omitSecondUnique, _) ((a, SecondUnique):as) = do
-    if omitSecondUnique then return () else putStrLn $ (replicate 1 delimiter) ++ a
+    if omitSecondUnique
+        then return ()
+        else putStrLn $ (replicate 1 delimiter) ++ a
     printComm delimiter omit as
 printComm delimiter omit@(_, _, omitCommon) ((a, Common):as) = do
-    if omitCommon then return () else putStrLn $ (replicate 2 delimiter) ++ a
+    if omitCommon
+        then return ()
+        else putStrLn $ (replicate 2 delimiter) ++ a
     printComm delimiter omit as
 
 extractCommonAndUnique :: (Ord a) => [a] -> [a] -> [(a, CommonOrUnique)]
 extractCommonAndUnique a [] = map (\x -> (x, FirstUnique)) a
 extractCommonAndUnique [] b = map (\x -> (x, SecondUnique)) b
 extractCommonAndUnique (a:as) (b:bs)
-    | a < b = (a, FirstUnique):extractCommonAndUnique as (b:bs)
-    | a > b = (b, SecondUnique):extractCommonAndUnique (a:as) bs
-    | otherwise = (a, Common):extractCommonAndUnique as bs
+    | a < b     = (a, FirstUnique)  : extractCommonAndUnique as (b:bs)
+    | a > b     = (b, SecondUnique) : extractCommonAndUnique (a:as) bs
+    | otherwise = (a, Common)   : extractCommonAndUnique as bs
