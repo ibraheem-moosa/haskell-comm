30022db9c5309128a19e90aefaed01e7de4bf6b2
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2018-02-20 10:10:43 +0600

    Instead of three lists we use one list of tagged values

--- Comm.hs
+++ Comm.hs
@@ -7,85 +7,44 @@ main :: IO ()
 main = do
         args <- getArgs
         let argLength = length args
-        let configuration = extractConfigurationFromArgs $ take (argLength - 2) args
         firstFileHandle <- openFile (last $ init args) ReadMode
         secondFileHandle <- openFile (last args) ReadMode
         firstFileContents <- hGetContents firstFileHandle
         secondFileContents <- hGetContents secondFileHandle
-        let (firstUnique, secondUnique, common) = extractCommonAndUnique (lines firstFileContents) (lines secondFileContents)
-        let a = if elem '1' configuration then [] else firstUnique 
-        let b = if elem '2' configuration then [] else secondUnique 
-        let c = if elem '3' configuration then [] else common
-        printComm '\t' (a, b, c)
+        let configuration = extractConfigurationFromArgs $ take (argLength - 2) args
+        let a = elem '1' configuration 
+        let b = elem '2' configuration
+        let c = elem '3' configuration
+        printComm '\t' (a, b, c) $ extractCommonAndUnique (lines firstFileContents) (lines secondFileContents)
 
 
 extractConfigurationFromArgs :: [String] -> String
 extractConfigurationFromArgs args = foldl (++) "" $ map tail $ filter (\x -> head x == '-') args
 
 
--- safe version of head
-getHead :: [a] -> Maybe a
-
-getHead = listToMaybe
+data CommonOrUnique = FirstUnique | SecondUnique | Common
 
 
--- checks if first parameter is less than second
--- Nothing is considered greater than any Just value
-lessthanWithNothing :: (Ord a) => (Maybe a) -> (Maybe a) -> (Maybe Bool)
+printComm :: Char -> (Bool, Bool, Bool) -> [(String, CommonOrUnique)] -> IO ()
 
-lessthanWithNothing Nothing Nothing = Nothing
-lessthanWithNothing (Just a) Nothing = Just True
-lessthanWithNothing Nothing (Just b) = Just False
-lessthanWithNothing (Just a) (Just b) = Just (a < b)
-
-
-andWithNothing :: (Maybe Bool) -> (Maybe Bool) -> Bool
-
-andWithNothing a b = (fromMaybe False a) && (fromMaybe False b)
-
-
-minHeadOfThreeLists :: (Ord a) => ([a], [a], [a]) -> Maybe (a, Int, ([a], [a], [a]))
-
-minHeadOfThreeLists (a, b, c)
-    | andWithNothing (lessthanWithNothing ah bh) (lessthanWithNothing ah ch) = Just (head a, 0, (tail a, b, c))
-    | andWithNothing (lessthanWithNothing bh ah) (lessthanWithNothing bh ch) = Just (head b, 1, (a, tail b, c))
-    | andWithNothing (lessthanWithNothing ch bh) (lessthanWithNothing ch bh) = Just (head c, 2, (a, b, tail c))
-    | otherwise = Nothing
-    where (ah, bh, ch) = (getHead a, getHead b, getHead c)
+printComm _ _ [] = return ()
+printComm delimiter omit@(omitFirstUnique, _, _) ((a, FirstUnique):as) = do
+                                                                    if omitFirstUnique then return () else putStrLn $ (replicate 0 delimiter) ++ a
+                                                                    printComm delimiter omit as
+printComm delimiter omit@(_, omitSecondUnique, _) ((a, SecondUnique):as) = do
+                                                                      if omitSecondUnique then return () else putStrLn $ (replicate 1 delimiter) ++ a
+                                                                      printComm delimiter omit as
+printComm delimiter omit@(_, _, omitCommon) ((a, Common):as) = do
+                                                           if omitCommon then return () else putStrLn $ (replicate 2 delimiter) ++ a
+                                                           printComm delimiter omit as
 
 
-printComm :: Char -> ([String], [String], [String]) -> IO ()
+extractCommonAndUnique :: (Ord a) => [a] -> [a] -> [(a, CommonOrUnique)]
 
-printComm delimiter lists =
-        case minHeadOfThreeLists lists of Nothing -> do
-                                                        return ()
-                                          Just (h, i, next) -> do
-                                                                  putStrLn $ (replicate i delimiter) ++ h
-                                                                  printComm delimiter next
-
-
-extractCommonAndUnique :: (Ord a) => [a] -> [a] -> ([a], [a], [a])
-
-extractCommonAndUnique a [] = (a, [], [])
-extractCommonAndUnique [] b = ([], b, [])
+extractCommonAndUnique a [] = map (\x -> (x, FirstUnique)) a
+extractCommonAndUnique [] b = map (\x -> (x, SecondUnique)) b
 extractCommonAndUnique (a:as) (b:bs)
-    | a < b = ((a:aRest1), bRest1, cRest1)
-    | a > b = (aRest2, (b:bRest2), cRest2)
-    | otherwise = (aRest3, bRest3, (a:cRest3))
-    where 
-        (aRest1, bRest1, cRest1) = extractCommonAndUnique as (b:bs)
-        (aRest2, bRest2, cRest2) = extractCommonAndUnique (a:as) bs
-        (aRest3, bRest3, cRest3) = extractCommonAndUnique as bs
+    | a < b = (a, FirstUnique):extractCommonAndUnique as (b:bs)
+    | a > b = (b, SecondUnique):extractCommonAndUnique (a:as) bs
+    | otherwise = (a, Common):extractCommonAndUnique as bs
 
-
-{-
-extractCommonAndUnique list1 list2 = extractCommonAndUniqueHelper list1 list2 ([], [], [])
-
-extractCommonAndUniqueHelper [] [] result = result
-extractCommonAndUniqueHelper [] (b:bs) (aUnique, bUnique, common) = extractCommonAndUniqueHelper [] bs (aUnique, (b:bUnique), common)
-extractCommonAndUniqueHelper (a:as) [] (aUnique, bUnique, common) = extractCommonAndUniqueHelper as [] ((a:aUnique), bUnique, common)
-extractCommonAndUniqueHelper (a:as) (b:bs) (aUnique, bUnique, common)
-    | (a < b) = extractCommonAndUniqueHelper as (b:bs) ((a:aUnique), bUnique, common)
-    | (a > b) = extractCommonAndUniqueHelper (a:as) bs (aUnique, (b:bUnique), common)
-    | otherwise = extractCommonAndUniqueHelper as bs (aUnique, bUnique, (a:common))
--}
