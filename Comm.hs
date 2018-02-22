ba3a5ef37277804d64a58fd00ce72d66b37666c9
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2018-02-22 11:52:38 +0600

    Factor out common code in printComm. Fixed indentation and whitespace following the commits of parsonsmatt.

--- Comm.hs
+++ Comm.hs
@@ -1,42 +1,69 @@
 import System.Environment
 import System.IO
 import Data.Maybe
+import Data.List
 
 
 main :: IO ()
 main = do
-        args <- getArgs
-        let argLength = length args
-        firstFileHandle <- openFile (last $ init args) ReadMode
-        secondFileHandle <- openFile (last args) ReadMode
-        firstFileContents <- hGetContents firstFileHandle
-        secondFileContents <- hGetContents secondFileHandle
-        let configuration = extractConfigurationFromArgs $ take (argLength - 2) args
-        let a = elem '1' configuration 
-        let b = elem '2' configuration
-        let c = elem '3' configuration
-        printComm '\t' (a, b, c) $ extractCommonAndUnique (lines firstFileContents) (lines secondFileContents)
+    args <- getArgs
+    let argLength = length args
+    firstFileHandle <- openFile (args !! (argLength - 2)) ReadMode
+    secondFileHandle <- openFile (last args) ReadMode
+    firstFileContents <- hGetContents firstFileHandle
+    secondFileContents <- hGetContents secondFileHandle
+    let configuration = extractConfigurationFromArgs
+                            $ take (argLength - 2) args
+    let a = elem '1' configuration
+    let b = elem '2' configuration
+    let c = elem '3' configuration
+    let d = case (elemIndex 'd' configuration)
+                of Nothing -> '\t'
+                   Just i -> head $ args !! (i + 1)
+    printComm d (a, b, c)
+        $ extractCommonAndUnique
+            (lines firstFileContents)
+            (lines secondFileContents)
 
 
 extractConfigurationFromArgs :: [String] -> String
-extractConfigurationFromArgs args = foldl (++) "" $ map tail $ filter (\x -> head x == '-') args
+extractConfigurationFromArgs args =
+    foldl (++) ""
+        $ map tail
+        $ filter (\x -> head x == '-') args
 
 
 data CommonOrUnique = FirstUnique | SecondUnique | Common
 
 
-printComm :: Char -> (Bool, Bool, Bool) -> [(String, CommonOrUnique)] -> IO ()
+shouldOmitCommonOrUnique :: (Bool, Bool, Bool) -> CommonOrUnique -> Bool
+
+shouldOmitCommonOrUnique (True, _, _) FirstUnique = True
+shouldOmitCommonOrUnique (_, True, _) SecondUnique = True
+shouldOmitCommonOrUnique (_, _, True) Common = True
+shouldOmitCommonOrUnique _ _ = False
+
+
+commonOrUniqueToColumn :: CommonOrUnique -> Int
+
+commonOrUniqueToColumn FirstUnique = 0
+commonOrUniqueToColumn SecondUnique = 1
+commonOrUniqueToColumn Common = 2
+
+
+printComm :: Char -> (Bool, Bool, Bool)
+                 -> [(String, CommonOrUnique)] -> IO ()
 
 printComm _ _ [] = return ()
-printComm delimiter omit@(omitFirstUnique, _, _) ((a, FirstUnique):as) = do
-                                                                    if omitFirstUnique then return () else putStrLn $ (replicate 0 delimiter) ++ a
-                                                                    printComm delimiter omit as
-printComm delimiter omit@(_, omitSecondUnique, _) ((a, SecondUnique):as) = do
-                                                                      if omitSecondUnique then return () else putStrLn $ (replicate 1 delimiter) ++ a
-                                                                      printComm delimiter omit as
-printComm delimiter omit@(_, _, omitCommon) ((a, Common):as) = do
-                                                           if omitCommon then return () else putStrLn $ (replicate 2 delimiter) ++ a
-                                                           printComm delimiter omit as
+printComm delimiter
+    omitConfiguration ((a, commonOrUnique):as) = do
+    if shouldOmitCommonOrUnique omitConfiguration commonOrUnique
+        then return ()
+        else putStrLn
+                 $ (replicate
+                        (commonOrUniqueToColumn commonOrUnique)
+                        delimiter) ++ a
+    printComm delimiter omitConfiguration as
 
 
 extractCommonAndUnique :: (Ord a) => [a] -> [a] -> [(a, CommonOrUnique)]
@@ -44,7 +71,7 @@ extractCommonAndUnique :: (Ord a) => [a] -> [a] -> [(a, CommonOrUnique)]
 extractCommonAndUnique a [] = map (\x -> (x, FirstUnique)) a
 extractCommonAndUnique [] b = map (\x -> (x, SecondUnique)) b
 extractCommonAndUnique (a:as) (b:bs)
-    | a < b = (a, FirstUnique):extractCommonAndUnique as (b:bs)
-    | a > b = (b, SecondUnique):extractCommonAndUnique (a:as) bs
+    | a < b     = (a, FirstUnique):extractCommonAndUnique as (b:bs)
+    | a > b     = (b, SecondUnique):extractCommonAndUnique (a:as) bs
     | otherwise = (a, Common):extractCommonAndUnique as bs
 
